c71f2788855b0473b4fdfd950d47ccdeb7372f62
xorrr <derxor@gmail.com>
2013-10-17 02:28:01 +0200

    set route and its nested route

--- js/app.js
+++ js/app.js
@@ -1,11 +1,7 @@
 App = Ember.Application.create();
 
 App.Router.map(function() {
-  // put your routes here
-});
-
-App.IndexRoute = Ember.Route.extend({
-  model: function() {
-    return ['red', 'yellow', 'blue'];
-  }
+  this.resource('bookmarks', function() {
+    this.resource('bookmark', { path: ':bookmark_id' });
+  });
 });

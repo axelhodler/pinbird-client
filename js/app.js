29c120b3789578daf153f3a07266eabe5560112e
xorrr <derxor@gmail.com>
2013-10-17 02:29:27 +0200

    add redirect from root to bookmarks

--- js/app.js
+++ js/app.js
@@ -5,3 +5,10 @@ App.Router.map(function() {
     this.resource('bookmark', { path: ':bookmark_id' });
   });
 });
+
+// If the root is accessed transition to /bookmarks
+App.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+    this.transitionTo('bookmarks');
+  }
+});

f3f22eca7d974d0c30412e567fc34183ecb4126c
xorrr <derxor@gmail.com>
2014-03-25 17:36:28 +0100

    modify runner.js to use the correct qunit files

--- tests/runner.js
+++ tests/runner.js
@@ -2,12 +2,15 @@ if (window.location.search.indexOf("?test") !== -1) {
   document.write(
     '<div id="qunit"></div>' +
     '<div id="qunit-fixture"></div>' +
+
     '<div id="ember-testing-container">' +
     '  <div id="ember-testing"></div>' +
     '</div>' +
+
     '<link rel="stylesheet" href="tests/runner.css">' +
-    '<link rel="stylesheet" href="tests/vendor/qunit-1.12.0.css">' +
-    '<script src="tests/vendor/qunit-1.12.0.js"></script>' +
+    '<link rel="stylesheet" href="bower_components/qunit/qunit/qunit.css">' +
+
+    '<script src="bower_components/qunit/qunit/qunit.js"></script>' +
     '<script src="tests/tests.js"></script>'
   )
 }

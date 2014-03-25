b0a79d772b89dc6a430842424cfbab793d792406
xorrr <derxor@gmail.com>
2014-03-25 17:51:49 +0100

    use setup and teardown test methods from the starter-kit

--- tests/tests.js
+++ tests/tests.js
@@ -1,5 +1,11 @@
 module("Test routing", {
   setup: function() {
+    // before each test, ensure the application is ready to run.
+    Ember.run(App, App.advanceReadiness);
+  },
+
+  teardown: function() {
+    // reset the application state between each test
     App.reset();
   }
 });

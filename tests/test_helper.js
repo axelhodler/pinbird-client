7dfb3dc8327dd49db4a47604cc572a87e77a5160
xorrr <derxor@gmail.com>
2014-03-25 23:27:42 +0100

    register helper to get the current path

--- tests/test_helper.js
+++ tests/test_helper.js
@@ -1,4 +1,10 @@
 App.rootElement = '#ember-testing';
 
+Ember.Test.registerHelper('getCurrentPath', function() {
+  var currentPath = App.__container__.lookup('controller:application')
+        .currentPath;
+  return currentPath;
+});
+
 App.setupForTesting();
 App.injectTestHelpers();

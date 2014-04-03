44c15b330476cdc5e94dc558d50294010bc14697
xorrr <derxor@gmail.com>
2014-04-04 00:55:10 +0200

    add handlebars task to Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,13 @@
 module.exports = function(grunt) {
+  grunt.initConfig({
+    handlebars: {
+      all: {
+        files: {
+          "js/templates.js": ["templates/*.handlebars"]
+        }
+      }
+    }
+  });
 
+  grunt.loadNpmTasks('grunt-contrib-handlebars');
 };
